bd69f812dacb1e89e22f4097b9b217ec6cdd4e19
JoeCoding3 <tiredredstoner@gmail.com>
2025-04-27 12:23:47 -0700

    modified:   games/[builtin util] Pack game dump.wpck 	new file:   games/dumps/[builtin util] Pack game dump/paths/ALL_PATHS_INFO_MAIN.csv 	modified:   wpck/filebuf.js 	modified:   wpck/libwpck.js

--- wpck/libwpck.js
+++ wpck/libwpck.js
@@ -59,24 +59,24 @@ function packWPCK (structure) {
     let fileSize = itemDataOffsets.at(-1) + itemBlocks.at(-1).dataLength
     let fileBufWriter = new FileBufWriter(new ArrayBuffer(fileSize))
         fileBufWriter.str(0x00, "ML3F-WPCK")
-        fileBufWriter.int(0x09, 0x01, {endian, size: IntSize.U8})
-        fileBufWriter.int(0x0A, items.length, {endian, size: IntSize.U16})
+        fileBufWriter.int(0x09, 0x01, endian, IntSize.U8)
+        fileBufWriter.int(0x0A, items.length, endian, IntSize.U16)
         for (let i = 0x00; i < items.length; i++) {
             let offset = itemBlockOffsets[i]
-                fileBufWriter.int(0x0C + (IntSize.U32 * i), offset, {endian, size: IntSize.U32})
+                fileBufWriter.int(0x0C + (IntSize.U32 * i), offset, endian, IntSize.U32)
             let dataOffset = itemDataOffsets[i]
             let itemBlock = itemBlocks[i]
-                fileBufWriter.int(offset, itemBlock.blockSize, {endian, size: IntSize.U8})
-                fileBufWriter.int(offset + 0x01, i, {endian, size: IntSize.U16})
-                fileBufWriter.int(offset + 0x03, itemBlock.pathLength, {endian, size: IntSize.U16})
-                for (let j = 0x00; j < itemBlock.pathLength; j++) fileBufWriter.int((offset + 0x05) + (j * IntSize.U16), itemBlock.path[j], {endian, size: IntSize.U16})
+                fileBufWriter.int(offset, itemBlock.blockSize, endian, IntSize.U8)
+                fileBufWriter.int(offset + 0x01, i, endian, IntSize.U16)
+                fileBufWriter.int(offset + 0x03, itemBlock.pathLength, endian, IntSize.U16)
+                for (let j = 0x00; j < itemBlock.pathLength; j++) fileBufWriter.int((offset + 0x05) + (j * IntSize.U16), itemBlock.path[j], endian, IntSize.U16)
                     offset += itemBlock.path.length * IntSize.U16
-                fileBufWriter.int(offset + 0x05, itemBlock.nameLength, {endian, size: IntSize.U8})
+                fileBufWriter.int(offset + 0x05, itemBlock.nameLength, endian, IntSize.U8)
                 fileBufWriter.str(offset + 0x06, itemBlock.name)
                     offset += itemBlock.nameLength
-                fileBufWriter.int(offset + 0x06, itemBlock.typeId, {endian, size: IntSize.U8})
-                fileBufWriter.int(offset + 0x07, itemBlock.dataLength, {endian, size: IntSize.U32})
-                fileBufWriter.int(offset + 0x0B, itemBlock.typeId == 0x00 ? dataOffset : 0x00, {endian, size: IntSize.U32})
+                fileBufWriter.int(offset + 0x06, itemBlock.typeId, endian, IntSize.U8)
+                fileBufWriter.int(offset + 0x07, itemBlock.dataLength, endian, IntSize.U32)
+                fileBufWriter.int(offset + 0x0B, itemBlock.typeId == 0x00 ? dataOffset : 0x00, endian, IntSize.U32)
             if (itemBlock.typeId == 0x00) fileBufWriter.buf(dataOffset, itemBlock.data)
         }
     let outBuf = fileBufWriter.data
@@ -87,38 +87,38 @@ function unpackWPCK (fileBuf) {
     let endian = Endian.BIG
     let magic = fileBuf.str(0x00, 0x09)
         FileBuf.expectVal(magic, `ML3F-WPCK`, `Invalid file magic (expected "ML3F-WPCK", got "${magic}")`)
-    let version = fileBuf.int(0x09, IntSize.U8, {endian})
+    let version = fileBuf.int(0x09, IntSize.U8, endian)
         FileBuf.expectVal(version, 0x01, `Invalid file version (expected 0x01, got ${version})`)
-    let itemCount = fileBuf.int(0x0A, IntSize.U16, {endian})
+    let itemCount = fileBuf.int(0x0A, IntSize.U16, endian)
     let itemOffsets = new Array(itemCount)
         let itemOffsetsBuf = fileBuf.buf(0x0C, itemCount * IntSize.U32)
         for (let i = 0x00; i < itemCount; i++) {
-            let itemOffset = itemOffsetsBuf.int(i * IntSize.U32, IntSize.U32, {endian})
+            let itemOffset = itemOffsetsBuf.int(i * IntSize.U32, IntSize.U32, endian)
             itemOffsets[i] = itemOffset
         }
     let items = new Array(itemCount)
         for (let i = 0x00; i < itemCount; i++) {
             let itemOffset = itemOffsets[i]
-            let blockSize = fileBuf.int(itemOffset, IntSize.U8, {endian})
+            let blockSize = fileBuf.int(itemOffset, IntSize.U8, endian)
             let block = fileBuf.buf(itemOffset + 0x01, blockSize)
-                let index = block.int(0x00, IntSize.U16, {endian})
-                let pathLength = block.int(0x02, IntSize.U16, {endian})
-                let pathBuf = block.buf(0x04, pathLength * IntSize.U16, {endian})
+                let index = block.int(0x00, IntSize.U16, endian)
+                let pathLength = block.int(0x02, IntSize.U16, endian)
+                let pathBuf = block.buf(0x04, pathLength * IntSize.U16, endian)
                     let path = new Array(pathLength)
                     for (let j = 0x00; j < pathLength; j++) {
-                        let pathPart = pathBuf.int(j * IntSize.U16, IntSize.U16, {endian})
+                        let pathPart = pathBuf.int(j * IntSize.U16, IntSize.U16, endian)
                         path[j] = pathPart
                     }
                     let curIndex = 0x04 + (pathLength * IntSize.U16)
-                let nameLength = block.int(0x00 + curIndex, IntSize.U8, {endian})
+                let nameLength = block.int(0x00 + curIndex, IntSize.U8, endian)
                 let name = block.str(0x01 + curIndex, nameLength)
                     curIndex += 0x01 + nameLength
-                let typeId = block.int(0x00 + curIndex, IntSize.U8, {endian})
+                let typeId = block.int(0x00 + curIndex, IntSize.U8, endian)
                     let type = null
                     if (typeId == 0x00) type = "FILE"
                     else if (typeId == 0x01) type = "FOLD"
-                let dataLength = block.int(0x01 + curIndex, IntSize.U32, {endian})
-                let dataOffset = block.int(0x05 + curIndex, IntSize.U32, {endian})
+                let dataLength = block.int(0x01 + curIndex, IntSize.U32, endian)
+                let dataOffset = block.int(0x05 + curIndex, IntSize.U32, endian)
                 let data = null
                     if (typeId == 0x00) data = fileBuf.buf(dataOffset, dataLength)
             items[index] = {
